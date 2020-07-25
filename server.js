97ac6c9907cda18a4f6a06fb8cf15620ed24b38b
Yi Lin Lee <yilinlee19@gmail.com>
2020-07-25 18:23:17 +1000

    add get and post notes

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
-////// Require data
+// Require data of existing notes from db.json
+const data = require("./db/db.json");
 
 // Set up the Express App
 // =============================================================
@@ -15,7 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
 // Routes
 // =============================================================
 
@@ -25,27 +26,41 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.get("/", function(req, res) {
 //     res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
-
 // app.get("/notes", function(req, res) {
 //     res.sendFile(path.join(__dirname, "public", "notes.html"));
 // });
-
 // app.get("*", function(_, res) {
 //     res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
-
 // API Routes
 // =============================================================
 
 app.get("/api/notes", function(req, res) {
-    return res.json(db.json);
+    return res.json(data);
 });
 
-// app.post("/api/notes", function(req, res) {
-//     console.log(req.body);
-//     db.json.push(req.body);
-//     console.log(db.json);
-// });
+app.post("/api/notes", function(req, res) {
+    console.log(req.body);
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    if(!newNote.title || !newNote.text){
+        res.status(400).json({ msg: 'Please include a title and text' });
+    }
+
+    // Add new note to "data" array and re-write the db.json file
+    data.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(data), function(error) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log("Successfully written to database")
+    })
+
+    // Update the saved notes on the DOM
+});
 
 // Starts the server to begin listening
 // =============================================================
